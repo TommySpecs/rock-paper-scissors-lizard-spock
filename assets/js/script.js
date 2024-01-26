3fced58fd949ffa2cdfa57f9b889d10175bef814
TommySpecs <>
2024-01-26 15:21:32 +0000

    Add pop up with rules, add image and notes to html

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $(".open").click(function () {
+        $(".pop-outer").fadeIn("slow");
+    });
+    $(".close").click(function () {
+        $(".pop-outer").fadeOut("slow");
+    });
+});
