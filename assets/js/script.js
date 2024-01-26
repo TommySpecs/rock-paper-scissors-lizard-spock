b86df6864e7b69f7dcd9fb9a7e6e5f0e7e81e641
TommySpecs <>
2024-01-26 21:09:55 +0000

    pop up box removal as currently not working, will revisit later

--- assets/js/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $(".open").click(function () {
-        $(".pop-outer").fadeIn("slow");
-    });
-    $(".close").click(function () {
-        $(".pop-outer").fadeOut("slow");
-    });
-});
