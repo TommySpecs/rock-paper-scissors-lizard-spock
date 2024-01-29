4044b58eefe08b68c80e7b23027cd37d00410720
TommySpecs <>
2024-01-29 19:25:50 +0000

    basic Rock, paper, Scissors, Lizard, Spock game made

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,22 @@
-// Rock Paper Scissors
+function playGame(playerChoice) {
+    var choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+    var computerChoice = choices[Math.floor(Math.random() * choices.length)];
 
-//Scissors beats Paper, Lizard
-//loses Spock, Rock
+    var result = '';
 
-//Rock beats Lizard, Scissors
-//loses Paper, Spock
-
-//Paper beats Rock, Spock
-//loses Scissors, Lizard
-
-//Lizard beats Paper, Spock
-//loses Scissors, Rock
-
-//Spock beats Scissors, Rock
-//loses Paper, Lizard
-
-//Modal Section//
-// Get the modal
-var modal = document.getElementById("rulesModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("rules-btn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function () {
-    modal.style.display = "block";
-};
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-    modal.style.display = "none";
-};
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (playerChoice === computerChoice) {
+        result = 'It\'s a tie!';
+    } else if (
+        (playerChoice === 'rock' && (computerChoice === 'scissors' || computerChoice === 'lizard')) ||
+        (playerChoice === 'paper' && (computerChoice === 'rock' || computerChoice === 'spock')) ||
+        (playerChoice === 'scissors' && (computerChoice === 'paper' || computerChoice === 'lizard')) ||
+        (playerChoice === 'lizard' && (computerChoice === 'spock' || computerChoice === 'paper')) ||
+        (playerChoice === 'spock' && (computerChoice === 'scissors' || computerChoice === 'rock'))
+    ) {
+        result = 'You win!';
+    } else {
+        result = 'You lose!';
     }
-};
+
+    document.getElementById('result').innerHTML = `You chose ${playerChoice}, computer chose ${computerChoice}. ${result}`;
+}
