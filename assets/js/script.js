5b583cfdf2f174887a474eca666714bc8ed26955
TommySpecs <>
2024-01-29 22:14:07 +0000

    Start script for losing a life

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+let life = 0;
+
 function playGame(playerChoice) {
     var choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
     var computerChoice = choices[Math.floor(Math.random() * choices.length)];
@@ -20,6 +22,14 @@ function playGame(playerChoice) {
 
     document.getElementById('result').innerHTML = `You chose ${playerChoice}, computer chose ${computerChoice}. ${result}`;
 }
+
+function loseLife() {
+    life = life -1;
+    if (life <1 ) {
+        result = `You lose try again? return to menu`;
+} 
+
+
 // Modal functions
 function openModal() {
     document.getElementById('rulesModal').style.display = 'block';
